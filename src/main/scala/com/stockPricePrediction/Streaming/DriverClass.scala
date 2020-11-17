2c1b4605234430c16d458252508faa6fc78e7f67
cheluvesha <cheluvesh.b@gmail.com>
2020-11-17 19:32:31 +0530

    [CHELUVESHA]Added: Comments

---
+++ src/main/scala/com/stockPricePrediction/Streaming/DriverClass.scala
@@ -0,0 +1,46 @@
+package com.stockPricePrediction.Streaming
+
+import com.stockPricePrediction.External.Producer.sendingDataToKafkaTopic
+import com.stockPricePrediction.External.ReadDataFromAlphaVantageAPI.parseDataToJson
+import com.stockPricePrediction.External.UtilityClass.createProducer
+import com.stockPricePrediction.External.{
+  ReadDataFromAlphaVantageAPI,
+  UtilityClass
+}
+import com.stockPricePrediction.Streaming.StockPricePredictionDStreams.{
+  creatingDStream,
+  predictStockClosePrice,
+  startStreaming
+}
+
+/***
+  * Driver Class responsible to trigger Data processing and predict price methods
+  * Dependencies Used Spark Core Api, Spark Sql, Spark MLLib, Spark Streaming Api, JSON Api,
+  * Kafka Client and ScapeGoat for Coverage
+ ***/
+object DriverClass extends App {
+  val sparkSession =
+    UtilityClass.createSparkSessionObj("Stock-Price-Prediction")
+  val topic = "StockPricePrediction"
+  val apiKey: String = System.getenv("API_KEY")
+  val companyName: String = "GOOG"
+  val url: String =
+    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=1min&symbol=" +
+      companyName + "&apikey=" + apiKey
+  val broker = "localhost:9092"
+  val groupId = "Group1"
+  val contentData = ReadDataFromAlphaVantageAPI.getApiContent(url)
+  val parsedData = parseDataToJson(contentData)
+  val createdProducer = createProducer(broker)
+  val status = sendingDataToKafkaTopic(topic, parsedData, createdProducer)
+  if (status == 1) {
+    println("Sending Data to Kafka topic is Successful")
+  } else {
+    println("Unable to Send Data to Kafka topic")
+  }
+  val kafkaParams = KafkaConsumerConfiguration.configureKafka(broker, groupId)
+  val messages = creatingDStream(topic, kafkaParams)
+  predictStockClosePrice(messages)
+  startStreaming()
+
+}
